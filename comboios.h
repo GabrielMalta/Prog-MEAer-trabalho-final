230b589bfedb469a9173e4aae9cf38ba3cbf0b6f
GabrielMalta <gabrielmalta@tecnico.ulisboa.pt>
2018-12-08 00:52:29 +0000

    leitor de configs completo; funfa com varias linhas e varios comboios

--- comboios.h
+++ comboios.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 // #include <SDL.h>
 
 #define DIMX 900
@@ -25,8 +26,8 @@
 #define BRANCO 9
 
 typedef struct{
-  char id[4];
-  float x, y;
+  char id[5];
+  int x, y;
   int cor;
   int tipo;
   float dim;
@@ -35,24 +36,41 @@ typedef struct{
 } PONTO;
 
 typedef struct node{
+  // char id[5];
   PONTO pt;
-  struct node *ant[3];
-  struct node *pr[3];
+  // struct node *ant[3];
+  struct node *pr;
+} LISTA_PONTOS;
+
+typedef struct {
+  char id[5];
+  LISTA_PONTOS *l;
 } LINHA;
 
-// typedef struct lista_linhas{
-//   LINHA l;
-//   struct lista_linhas *pr;
-// } LISTA LINHAS;
-
-typedef struct comboio{
-  char id[2];
+typedef struct{
+  char id[3];
   int dim;
-  float r_bolas;
+  int r_bolas;
   int cor[4];
+  char orig_l[5];
+  char orig_pt[5];
+  char dest_l[5];
+  char dest_pt[5];
 } COMBOIO;
 
-// typedef struct lista_comboios
-// }
+// typedef struct lista_comboios{
+//   COMBOIO train;
+//   struct lista_comboios *pr;
+// } LISTA_COMBOIOS;
 
 void mostra_ponto(PONTO pt);
+
+void mostra_boio(COMBOIO boio);
+
+int numero_cor(char string[]);
+
+int numero_tipo(char string[]);
+
+char * cor_numero(int no);
+
+void leitor_configs(COMBOIO ***comboios, LINHA ***linhas, int *dim_X, int *dim_Y);
--- main.c
+++ main.c
@@ -1,3 +1,26 @@
-// ola jacob
+// gcc -Wall -pedantic -c comboios.c; gcc -Wall -pedantic -c main.c; gcc main.o comboios.o -o comboios
+
 #include "comboios.h"
-ola
+
+int main(void){
+  LINHA **linha = NULL;
+  LISTA_PONTOS *ap;
+  COMBOIO **train = NULL;
+  int dimensaoX, dimensaoY;
+  int i;
+
+  leitor_configs(&train, &linha, &dimensaoX, &dimensaoY);
+  printf("cheguei\n");
+  fflush(stdout);
+  for(i=0; linha[i] !=NULL; i++){
+    for(ap = linha[i]->l; ap!= NULL; ap=ap->pr){
+      mostra_ponto(ap->pt);
+    }
+  }
+
+  for(i=0; train[i] != NULL; i++){
+    mostra_boio( *(train[i]));
+  }
+
+  return 0;
+}
