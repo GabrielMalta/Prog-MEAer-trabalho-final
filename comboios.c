970942f3185f52d4a0dd4e37a8f652eb4ce0c9f2
gtjacob <gongas.gomaro@gmail.com>
2018-12-23 11:32:22 +0000

    eyet

--- comboios.c
+++ comboios.c
@@ -1,68 +1,5 @@
 #include "comboios.h"
 
-void mostra_ponto(PONTO pt){
-  char tipo[8];
-  switch(pt.tipo){
-    case 0: strcpy(tipo, "Erro");      break;
-    case 1: strcpy(tipo, "Estacao");  break;
-    case 2: strcpy(tipo, "Entrada");  break;
-    case 3: strcpy(tipo, "Saida");    break;
-    case 4: strcpy(tipo, "Via");    break;
-    default: printf("Erro traducao de tipo\n"); break;
-  }
-  printf("\nPonto: %s\nTipo: %s\nCor: %s\n", pt.id, tipo, cor_codigo(pt.cor));
-  fflush (stdout);
-}
-
-int numero_tipo(char string[]){ //funciona
-  // converte a string de tipo num inteiro para armazenar
-  if (strcmp("EST", string) == 0) return 1;
-  if (strcmp("ENT", string) == 0) return 2;
-  if (strcmp("SAI", string) == 0) return 3;
-  if (strcmp("VIA", string) == 0) return 4;
-  return 0;
-}
-
-void mostra_boio(COMBOIO boio){
-  printf("\n/----COMBOIO %s----/\n", boio.id);
-  printf("Dimensao:%d\n", boio.dim);
-  printf("Cor locom.:%s\n", cor_codigo(boio.cor));
-  printf("Ponto origem:%s\n", boio.origem->pt.id);
-  printf("Tempo de spawn:%.2f\n", boio.tempo_spawn);
-  printf("Velocidade:%.2fpixeis/segundo\n", boio.veloc*FPS);
-  fflush(stdout);
-}
-
-void liga_pontos(char aux_string[6][10], LISTA_LINHAS *topo_lista_linhas){
-  LISTA_PONTOS *pt1 = NULL, *pt2 = NULL;
-
-  pt1 = procura_ponto(aux_string[0], aux_string[1], topo_lista_linhas);
-  pt2 = procura_ponto(aux_string[2], aux_string[3], topo_lista_linhas);
-  if (pt1 == NULL || pt2 == NULL){
-    printf("ERRO de ligacao de pontos\n");
-    fflush (stdout);
-    exit(0);
-  }
-  pt1->pr[1]=pt2;
-  // mostra_ponto(pt1->pt);
-  // mostra_ponto(pt1->pr[1]->pt);
-}
-
-LISTA_PONTOS * procura_ponto(char *id_linha, char *id_ponto, LISTA_LINHAS *topo_lista_linhas){
-  LISTA_PONTOS *aux_pt;
-  LISTA_LINHAS *linha_atual;
-
-  for(linha_atual=topo_lista_linhas; linha_atual!=NULL; linha_atual=linha_atual->pr){
-    if (strcmp(id_linha, linha_atual->linha.id) == 0){
-      for(aux_pt=linha_atual->linha.l; aux_pt != NULL; aux_pt=aux_pt->pr[0]){
-        if(strcmp(id_ponto, aux_pt->pt.id) == 0){
-          return aux_pt;
-        }
-      }
-    }
-  }
-  return NULL;
-}
 
 LISTA_GRAF_BOIO * mexe_comboios2(LISTA_GRAF_BOIO *lista_graf_boios){
   float deltaX, deltaY;
@@ -135,48 +72,9 @@ LISTA_GRAF_BOIO * mexe_comboios2(LISTA_GRAF_BOIO *lista_graf_boios){
   return lista_graf_boios;
 }
 
-void mostra_boios_ativos(LISTA_GRAF_BOIO *lista_graf_boios){
-  int i=1;
-  while(lista_graf_boios!=NULL){
-    printf("\nGRAF_BOIO #%d\n", i++);
-    printf("Prototipo:%s\n", lista_graf_boios->graf.boio->id);
-    printf("Cor locom.:%s\n", cor_codigo(lista_graf_boios->graf.cor[0]));
-    printf("X=%7.2fY=%7.2f\n", lista_graf_boios->graf.x[0], lista_graf_boios->graf.y[0]);
-    fflush(stdout);
-    lista_graf_boios=lista_graf_boios->pr;
-  }
-}
-
-LISTA_PONTOS * procura_ponto_por_coords(LISTA_LINHAS *topo_lista_linhas, int x, int y){
-  LISTA_PONTOS *aux=NULL;
-  int deltaX, deltaY;
-
-  for(; topo_lista_linhas!=NULL; topo_lista_linhas=topo_lista_linhas->pr){
-
-    for(aux=topo_lista_linhas->linha.l; aux!=NULL; aux=aux->pr[0]){
-      deltaX = aux->pt.x - x;
-      deltaY = aux->pt.y - y;
-      if(deltaX*deltaX + deltaY*deltaY < RAIO_ESTACAO*RAIO_ESTACAO){
-        return aux;
-      }
-    }
-  }
-  return NULL;
-}
-
-LISTA_GRAF_BOIO * procura_locomotiva_por_coords(LISTA_GRAF_BOIO *graf_boios, int x, int y){
-  for(; graf_boios!=NULL; graf_boios=graf_boios->pr){
-    if (pow(x - graf_boios->graf.x[0],2) + pow(y- graf_boios->graf.y[0],2) < pow(RAIO_ESTACAO,2)){
-      return graf_boios;
-    }
-  }
-  return NULL;
-}
-
 void opcao_mostra_comboio(LISTA_COMBOIOS *topo_lista_comboios){
   char comboio[100];
   int i=0;
-  // char leitura[100];
   LISTA_COMBOIOS *aux=topo_lista_comboios;
     while(1){
       system("clear");
@@ -243,7 +141,6 @@ LISTA_COMBOIOS * opcao_elimina_comboio(LISTA_COMBOIOS *topo_lista_comboios){
       }
       if(i==0)
         printf("Erro, comboio inexistente\n");
-      getchar();
       while(getchar()!='\n');
     }
     return topo_lista_comboios;
@@ -333,7 +230,6 @@ LISTA_LINHAS * opcao_elimina_linha(LISTA_LINHAS *topo_lista_linhas){
       }
       if (i==0)
         printf("Erro, linha inexistente\n");
-      getchar();
       while(getchar()!='\n');
   }
   return topo_lista_linhas;
@@ -363,10 +259,112 @@ LISTA_COMBOIOS * opcao_novo_comboio(LISTA_COMBOIOS *topo_lista_comboios, LISTA_L
   return topo_lista_comboios;
 }
 
+LISTA_GRAF_BOIO * procura_locomotiva_por_coords(LISTA_GRAF_BOIO *graf_boios, int x, int y){
+  for(; graf_boios!=NULL; graf_boios=graf_boios->pr){
+    if (pow(x - graf_boios->graf.x[0],2) + pow(y- graf_boios->graf.y[0],2) < pow(RAIO_ESTACAO,2)){
+      return graf_boios;
+    }
+  }
+  return NULL;
+}
+
+LISTA_PONTOS * procura_ponto_por_coords(LISTA_LINHAS *topo_lista_linhas, int x, int y){
+  LISTA_PONTOS *aux=NULL;
+  int deltaX, deltaY;
+
+  for(; topo_lista_linhas!=NULL; topo_lista_linhas=topo_lista_linhas->pr){
+
+    for(aux=topo_lista_linhas->linha.l; aux!=NULL; aux=aux->pr[0]){
+      deltaX = aux->pt.x - x;
+      deltaY = aux->pt.y - y;
+      if(deltaX*deltaX + deltaY*deltaY < RAIO_ESTACAO*RAIO_ESTACAO){
+        return aux;
+      }
+    }
+  }
+  return NULL;
+}
+
 void toggle_andamento_comboio(LISTA_GRAF_BOIO *boio_a_parar){
   if (boio_a_parar->graf.veloc != 0){
     boio_a_parar->graf.veloc =0;
     return;
   }
   boio_a_parar->graf.veloc=boio_a_parar->graf.boio->veloc;
+}
+
+void mostra_boios_ativos(LISTA_GRAF_BOIO *lista_graf_boios){
+  int i=1;
+  while(lista_graf_boios!=NULL){
+    printf("\nGRAF_BOIO #%d\n", i++);
+    printf("Prototipo:%s\n", lista_graf_boios->graf.boio->id);
+    printf("Cor locom.:%s\n", cor_codigo(lista_graf_boios->graf.cor[0]));
+    printf("X=%7.2fY=%7.2f\n", lista_graf_boios->graf.x[0], lista_graf_boios->graf.y[0]);
+    fflush(stdout);
+    lista_graf_boios=lista_graf_boios->pr;
+  }
+}
+
+void mostra_ponto(PONTO pt){
+  char tipo[8];
+  switch(pt.tipo){
+    case 0: strcpy(tipo, "Erro");      break;
+    case 1: strcpy(tipo, "Estacao");  break;
+    case 2: strcpy(tipo, "Entrada");  break;
+    case 3: strcpy(tipo, "Saida");    break;
+    case 4: strcpy(tipo, "Via");    break;
+    default: printf("Erro traducao de tipo\n"); break;
+  }
+  printf("\nPonto: %s\nTipo: %s\nCor: %s\n", pt.id, tipo, cor_codigo(pt.cor));
+  fflush (stdout);
+}
+
+int numero_tipo(char string[]){ //funciona
+  // converte a string de tipo num inteiro para armazenar
+  if (strcmp("EST", string) == 0) return 1;
+  if (strcmp("ENT", string) == 0) return 2;
+  if (strcmp("SAI", string) == 0) return 3;
+  if (strcmp("VIA", string) == 0) return 4;
+  return 0;
+}
+
+void mostra_boio(COMBOIO boio){
+  printf("\n/----COMBOIO %s----/\n", boio.id);
+  printf("Dimensao:%d\n", boio.dim);
+  printf("Cor locom.:%s\n", cor_codigo(boio.cor));
+  printf("Ponto origem:%s\n", boio.origem->pt.id);
+  printf("Tempo de spawn:%.2f\n", boio.tempo_spawn);
+  printf("Velocidade:%.2fpixeis/segundo\n", boio.veloc*FPS);
+  fflush(stdout);
+}
+
+void liga_pontos(char aux_string[6][10], LISTA_LINHAS *topo_lista_linhas){
+  LISTA_PONTOS *pt1 = NULL, *pt2 = NULL;
+
+  pt1 = procura_ponto(aux_string[0], aux_string[1], topo_lista_linhas);
+  pt2 = procura_ponto(aux_string[2], aux_string[3], topo_lista_linhas);
+  if (pt1 == NULL || pt2 == NULL){
+    printf("ERRO de ligacao de pontos\n");
+    fflush (stdout);
+    exit(0);
+  }
+  pt1->pr[1]=pt2;
+  // mostra_ponto(pt1->pt);
+  // mostra_ponto(pt1->pr[1]->pt);
+}
+
+LISTA_PONTOS * procura_ponto(char *id_linha, char *id_ponto, LISTA_LINHAS *topo_lista_linhas){
+  LISTA_PONTOS *aux_pt;
+  LISTA_LINHAS *linha_atual;
+
+  for(linha_atual=topo_lista_linhas; linha_atual!=NULL; linha_atual=linha_atual->pr){
+    if (strcmp(id_linha, linha_atual->linha.id) == 0){
+      for(aux_pt=linha_atual->linha.l; aux_pt != NULL; aux_pt=aux_pt->pr[0]){
+        if(strcmp(id_ponto, aux_pt->pt.id) == 0){
+          return aux_pt;
+        }
+      }
+    }
+  }
+  return NULL;
 }
