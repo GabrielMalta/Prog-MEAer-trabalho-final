f02dc9976715b9dd3fb4c59714f0a4d4b4764567
gtjacob <gongas.gomaro@gmail.com>
2018-12-24 16:41:20 +0000

    sad

--- comboios.h
+++ comboios.h
@@ -95,23 +95,59 @@ typedef struct lista_graf_boios{
   struct lista_graf_boios *pr;
 } LISTA_GRAF_BOIO;
 
+void leitor_configs( LISTA_COMBOIOS **comboios, LISTA_LINHAS **linhas, int *dim_X, int *dim_Y, char *nome_ficheiro);
+
+LISTA_COMBOIOS * preenche_comboio(char *aux_string, int *aux_int, LISTA_COMBOIOS *topo_lista_comboios, LISTA_LINHAS *topo_lista_linhas);
+
+void preenche_linha(char *aux_string, int *aux_int, LISTA_LINHAS * nova_linha, LISTA_PONTOS **atual);
+
+void liga_pontos(char aux_string[6][10], LISTA_LINHAS *topo_lista_linhas);
+
+LISTA_PONTOS * procura_ponto(char *id_linha, char *id_ponto, LISTA_LINHAS *topo_lista_linhas);
+
+int numero_tipo( char string[]);
+
+void opcao_mostra_linha(LISTA_LINHAS *topo_lista_linhas);
+
+LISTA_LINHAS * opcao_elimina_linha(LISTA_LINHAS *topo_lista_linhas);
+
+void opcao_mostra_comboio(LISTA_COMBOIOS *topo_lista_comboios);
+
+LISTA_COMBOIOS * opcao_elimina_comboio(LISTA_COMBOIOS *topo_lista_comboios);
+
+LISTA_COMBOIOS * opcao_novo_comboio(LISTA_COMBOIOS *topo_lista_comboios, LISTA_LINHAS * topo_lista_linhas);
+
+int verifica(int n_min, int n_max, char *texto);
+
+void get_id_comboio(char *id_comboio);
+
+void get_coords_origem(char * linha, char *ponto, LISTA_LINHAS * topo_lista_linhas);
+
+void mostra_linha(LISTA_LINHAS* topo_lista_linhas, char * texto, char*linha);
+
 void mostra_ponto( PONTO pt);
 
 void mostra_boio( COMBOIO boio);
 
 Uint32 codigo_cor( char string[]);
 
-int numero_tipo( char string[]);
+char * cor_para_string(int cor);
 
 Uint32 cor_converte(int cor);
 
 char * cor_codigo( Uint32 no);
 
-void leitor_configs( LISTA_COMBOIOS **comboios, LISTA_LINHAS **linhas, int *dim_X, int *dim_Y, char *nome_ficheiro);
-
-void liga_pontos(char aux_string[6][10], LISTA_LINHAS *topo_lista_linhas);
+void simular(LISTA_COMBOIOS *topo_lista_comboios, LISTA_LINHAS *topo_lista_linhas, int dimensaoX, int dimensaoY);
 
 int inicializa_janela( int dim_X, int dim_Y);
+
+LISTA_GRAF_BOIO *gera_novos_graf_boios(LISTA_GRAF_BOIO *lista_graf_boios, LISTA_COMBOIOS *comboios, int ticks_simulacao);
+
+LISTA_GRAF_BOIO *cria_grafico_do_comboio(LISTA_GRAF_BOIO *lista_graf_boios, COMBOIO *comboio);
+
+LISTA_GRAF_BOIO * mexe_comboios2(LISTA_GRAF_BOIO *lista_graf_boios);
+
+LISTA_GRAF_BOIO * remove_graf_boio(LISTA_GRAF_BOIO *lista_graf_boios, LISTA_GRAF_BOIO *eliminar);
 
 void atualiza_render(LISTA_LINHAS *topo_lista_linhas, LISTA_GRAF_BOIO *boios_graficos, int dimX, int dimY, int pausa);
 
@@ -121,53 +157,17 @@ void desenha_ligacoes(LISTA_LINHAS *linha);
 
 void desenha_comboios(LISTA_GRAF_BOIO *lista_graf_boios);
 
-LISTA_PONTOS * procura_ponto(char *id_linha, char *id_ponto, LISTA_LINHAS *topo_lista_linhas);
-
-LISTA_GRAF_BOIO *cria_grafico_do_comboio(LISTA_GRAF_BOIO *lista_graf_boios, COMBOIO *comboio);
-
-LISTA_GRAF_BOIO *gera_novos_graf_boios(LISTA_GRAF_BOIO *lista_graf_boios, LISTA_COMBOIOS *comboios, int ticks_simulacao);
-
-LISTA_GRAF_BOIO * mexe_comboios2(LISTA_GRAF_BOIO *lista_graf_boios);
-
-LISTA_GRAF_BOIO * remove_graf_boio(LISTA_GRAF_BOIO *lista_graf_boios, LISTA_GRAF_BOIO *eliminar);
-
-LISTA_PONTOS * procura_ponto_por_coords(LISTA_LINHAS *topo_lista_linhas, int x, int y);
-
-LISTA_GRAF_BOIO * procura_locomotiva_por_coords(LISTA_GRAF_BOIO *graf_boios, int x, int y);
+void desenha_botoes(int dimX, int dimY, int pausa);
 
 int eventos_sdl(SDL_Event *event, LISTA_LINHAS *topo_lista_linhas, LISTA_GRAF_BOIO *topo_lista_graf_boios, int dimX, int dimY);
 
-void opcao_mostra_comboio(LISTA_COMBOIOS *topo_lista_comboios);
-
-LISTA_COMBOIOS * opcao_elimina_comboio(LISTA_COMBOIOS *topo_lista_comboios);
-
-void opcao_mostra_linha(LISTA_LINHAS *topo_lista_linhas);
-
-LISTA_LINHAS * opcao_elimina_linha(LISTA_LINHAS *topo_lista_linhas);
-
-void simular(LISTA_COMBOIOS *topo_lista_comboios, LISTA_LINHAS *topo_lista_linhas, int dimensaoX, int dimensaoY);
-
-LISTA_COMBOIOS * opcao_novo_comboio(LISTA_COMBOIOS *topo_lista_comboios, LISTA_LINHAS * topo_lista_linhas);
-
-void desenha_botoes(int dimX, int dimY, int pausa);
-
 int carregou_botao(int dimX, int dimY, int x, int y);
 
+LISTA_GRAF_BOIO * procura_locomotiva_por_coords(LISTA_GRAF_BOIO *graf_boios, int x, int y);
+
+LISTA_PONTOS * procura_ponto_por_coords(LISTA_LINHAS *topo_lista_linhas, int x, int y);
+
 void toggle_andamento_comboio(LISTA_GRAF_BOIO *boio_a_parar);
-
-LISTA_COMBOIOS * preenche_comboio(char *aux_string, int *aux_int, LISTA_COMBOIOS *topo_lista_comboios, LISTA_LINHAS *topo_lista_linhas);
-
-void preenche_linha(char *aux_string, int *aux_int, LISTA_LINHAS * nova_linha, LISTA_PONTOS **atual);
-
-int verifica(int n_min, int n_max, char *texto);
-
-char * cor_para_string(int cor);
-
-void mostra_linha(LISTA_LINHAS* topo_lista_linhas, char * texto, char*linha);
-
-void get_id_comboio(char *id_comboio);
-
-void get_coords_origem(char * linha, char *ponto, LISTA_LINHAS * topo_lista_linhas);
 
 // void mostra_boios_ativos(LISTA_GRAF_BOIO *lista_graf_boios);
 
